6d8700d79e2d5547d206b68736ad21474d4e2e01
Lemo Trano <tran1219@umn.edu>
2024-02-07 17:14:45 -0600

    Added template code for Task 1

---
+++ components/CustomButton.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { View } from "react-native";
+// Put any other neccessary imports here
+
+
+// Define your types and props here
+
+
+// Put your function here
+
+const CustomButton = () => {
+
+    return (
+        <View></View>
+    );
+};
+
+
+
+
+
+// Put your styles down here
+
+export default CustomButton;
