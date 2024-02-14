5fd6ed4e4f86bc21e4cf4d9ec58d585f7461df4f
AureliusNguyen <nguy5272@umn.edu>
2024-02-13 18:35:57 -0600

    need to clean branch

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Gopher Tunnels</Text>
+      <Text>home.tsx</Text>
       <StatusBar style="auto" />
     </View>
   );
