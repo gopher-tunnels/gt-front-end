f449325091513c8133e76258b763ff63ad96140c
Victor Hofstetter <victorbarroshof@gmail.com>
2024-05-08 11:57:04 -0500

    upgrade to Expo SDK 51

--- app/Home/index.tsx
+++ app/Home/index.tsx
@@ -1,20 +1,25 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from 'react';
 import { Alert, Platform, Text, TouchableOpacity, View } from 'react-native';
-import MapView, { MapPolyline, Marker, PROVIDER_DEFAULT, PROVIDER_GOOGLE, Polyline } from "react-native-maps";
-import * as Location from "expo-location";
-import LocationButton from "../../components/LocationButton";
-import { LocationObject as LocationObjectLocation, requestForegroundPermissionsAsync as requestForegroundPermissionsAsyncLocation, watchHeadingAsync as watchHeadingAsyncLocation, LocationSubscription as LocationSubscriptionLocation } from 'expo-location';
+import MapView, {
+  MapPolyline,
+  Marker,
+  PROVIDER_DEFAULT,
+  PROVIDER_GOOGLE,
+  Polyline,
+} from 'react-native-maps';
+import * as Location from 'expo-location';
+import LocationButton from '../../components/LocationButton';
+import {
+  LocationObject as LocationObjectLocation,
+  requestForegroundPermissionsAsync as requestForegroundPermissionsAsyncLocation,
+  watchHeadingAsync as watchHeadingAsyncLocation,
+  LocationSubscription as LocationSubscriptionLocation,
+} from 'expo-location';
 import { DeviceMotion } from 'expo-sensors';
 
-import { 
-  Container,
-  Map,
-  LocButton
-} from './styles'
-import Splash from "../Splash";
-import { LocationSubscriber } from "expo-location/build/LocationSubscribers";
-
-
+import { Container, Map, LocButton } from './styles';
+import Splash from '../Splash';
+import { LocationSubscriber } from 'expo-location/build/LocationSubscribers';
 
 export interface types {
   newText: string;
@@ -22,7 +27,9 @@ export interface types {
 
 export default function Home() {
   // State to get the current user location
-  const [location, setLocation] = useState<Location.LocationObject | null>(null);
+  const [location, setLocation] = useState<Location.LocationObject | null>(
+    null,
+  );
   // State to get the current location of the current user view
   const [region, setRegion] = useState({
     latitude: 44.97565862446892,
@@ -36,7 +43,7 @@ export default function Home() {
   const [markerSize, setMarkerSize] = useState(56);
   const [isPermissionGranted, setIsPermissionGranted] = useState(false);
   const mapRef = useRef<any>();
-  
+
   const focusMap = () => {
     if (location) {
       const { latitude, longitude } = location.coords;
@@ -46,14 +53,14 @@ export default function Home() {
         latitudeDelta: 0.02,
         longitudeDelta: 0.02,
       });
-    };
+    }
   };
-
 
   useEffect(() => {
     const getLocation = async () => {
       try {
-        const { status: permissionStatus } = await Location.requestForegroundPermissionsAsync();
+        const { status: permissionStatus } =
+          await Location.requestForegroundPermissionsAsync();
         if (permissionStatus !== 'granted') {
           console.log('Location permission not granted');
           return;
@@ -64,7 +71,6 @@ export default function Home() {
       } catch (error) {
         console.error('Error getting location:', error);
       }
-
     };
 
     // Call getLocation initially
@@ -76,10 +82,9 @@ export default function Home() {
   useEffect(() => {
     const getHeading = async () => {
       try {
-        Location.watchHeadingAsync(newHeading => {
+        Location.watchHeadingAsync((newHeading) => {
           setHeading(newHeading.trueHeading);
-        }); 
-
+        });
       } catch (error) {
         console.error('Error getting heading:', error);
       }
@@ -97,60 +102,84 @@ export default function Home() {
     setRegion(newRegion);
   };
 
-  
   if (!location) {
-    return <Splash/>
-  } 
+    return <Splash />;
+  }
 
   return (
     <Container>
       {location && (
-      <><Map
-          // Specifies the location of the initial view screen
-          initialRegion={region}
-          // OnRegionChangeComplete runs when the user stops dragging the map view, our callback function uses this and sets the current region to the location of the screen where the user stopped
-          onRegionChange={(region) => { handleRegionChange(region); } }
-          showsUserLocation={false}
-          showsMyLocationButton={false}
-          showsBuildings={true}
-          userInterfaceStyle="light"
-          provider={PROVIDER_GOOGLE}
-
-          ref={mapRef}
-        >
-          <LocButton
-            coordinate={{
-              latitude: location.coords.latitude,
-              longitude: location.coords.longitude,
+        <>
+          <Map
+            // Specifies the location of the initial view screen
+            initialRegion={region}
+            // OnRegionChangeComplete runs when the user stops dragging the map view, our callback function uses this and sets the current region to the location of the screen where the user stopped
+            onRegionChange={(region) => {
+              handleRegionChange(region);
             }}
-            rotation={heading || 0} // Set the rotation of the marker based on the user's heading
-            onPress={focusMap}
-            anchor={{ x: 0.5, y: 0.5 }}
+            showsUserLocation={false}
+            showsMyLocationButton={false}
+            showsBuildings={true}
+            userInterfaceStyle="light"
+            provider={
+              Platform.OS === 'android' ? PROVIDER_GOOGLE : PROVIDER_DEFAULT
+            }
+            ref={mapRef}
           >
-            <LocationButton width={70} height={70} />
-          </LocButton>
-          <MapPolyline
-            coordinates={[
-              {latitude: location.coords.latitude, longitude: location.coords.longitude},
-              {latitude: 44.97565862446892, longitude: -93.23372512269837},
-              {latitude: 44.97565862446895, longitude: -93.23372512269835}
-            ]}
-            strokeWidth={5}
-            geodesic = {true}
-            
-          ></MapPolyline>
-        </Map>
-        <TouchableOpacity onPress={focusMap} style={{ position: 'absolute', bottom: 20, right: 20, backgroundColor: 'white', padding: 10, borderRadius: 10 }}>
-          <Text>Focus to Current Location</Text>
-        </TouchableOpacity>
-      </>
+            <LocButton
+              coordinate={{
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+              }}
+              rotation={heading || 0} // Set the rotation of the marker based on the user's heading
+              onPress={focusMap}
+              anchor={{ x: 0.5, y: 0.5 }}
+            >
+              <LocationButton width={70} height={70} />
+            </LocButton>
+            <MapPolyline
+              coordinates={[
+                {
+                  latitude: location.coords.latitude,
+                  longitude: location.coords.longitude,
+                },
+                { latitude: 44.97565862446892, longitude: -93.23372512269837 },
+                { latitude: 44.97565862446895, longitude: -93.23372512269835 },
+              ]}
+              strokeWidth={5}
+              geodesic={true}
+            ></MapPolyline>
+          </Map>
+          <TouchableOpacity
+            onPress={focusMap}
+            style={{
+              position: 'absolute',
+              bottom: 20,
+              right: 20,
+              backgroundColor: 'white',
+              padding: 10,
+              borderRadius: 10,
+            }}
+          >
+            <Text>Focus to Current Location</Text>
+          </TouchableOpacity>
+        </>
       )}
-      <Text style={{fontFamily: "Plus-Jakarta-Sans"}}>heading: {heading !== null ? heading.toFixed(2) : 'Loading...'}</Text>
-      <Text style={{fontFamily: "Plus-Jakarta-Sans"}}>Current user latitude: {location.coords.latitude}</Text>
-      <Text style={{fontFamily: "Plus-Jakarta-Sans"}}>Current user longitude: {location.coords.longitude}</Text>
-      <Text style={{fontFamily: "Plus-Jakarta-Sans"}}>Current latitude: {region.latitude}</Text>
-      <Text style={{fontFamily: "Plus-Jakarta-Sans"}}>Current longitude: {region.longitude}</Text>
+      <Text style={{ fontFamily: 'Plus-Jakarta-Sans' }}>
+        heading: {heading !== null ? heading.toFixed(2) : 'Loading...'}
+      </Text>
+      <Text style={{ fontFamily: 'Plus-Jakarta-Sans' }}>
+        Current user latitude: {location.coords.latitude}
+      </Text>
+      <Text style={{ fontFamily: 'Plus-Jakarta-Sans' }}>
+        Current user longitude: {location.coords.longitude}
+      </Text>
+      <Text style={{ fontFamily: 'Plus-Jakarta-Sans' }}>
+        Current latitude: {region.latitude}
+      </Text>
+      <Text style={{ fontFamily: 'Plus-Jakarta-Sans' }}>
+        Current longitude: {region.longitude}
+      </Text>
     </Container>
   );
-};
-
+}
