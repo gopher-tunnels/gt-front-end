3296dd72d9fbf18cde5cb47217816af9c96a0bdf
Victor Hofstetter <victorbarroshof@gmail.com>
2024-06-09 22:01:06 -0500

    create CustomButton component

--- components/CustomButton/styles.ts
+++ components/CustomButton/styles.ts
@@ -1,15 +1,39 @@
-import styled from "styled-components/native";
+import { MotiView } from 'moti';
+import styled from 'styled-components/native';
 
-export const Container = styled.TouchableOpacity`
-    align-items: center;
-    justify-content: center;
-    padding-vertical: 10px;
-    padding-horizontal: 32px;
-    background-color: #a881af;
-    border-radius: 10px;
+interface ContainerProps {
+  variant?: 'filled' | 'outlined';
+  loading?: boolean;
+}
+
+export const Container = styled.Pressable<ContainerProps>`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ variant, theme }) =>
+    !variant || variant === 'filled'
+      ? theme.colors.primaryMain
+      : 'transparent'};
+  border: ${({ variant, theme }) =>
+    !variant || variant === 'filled'
+      ? 'none'
+      : `1px solid ${theme.colors.primaryMain}`};
+  max-width: 360px;
+  height: 48px;
+  border-radius: 8px;
+  margin: 0 16px;
+  gap: ${({ loading }) => (loading ? 12 : 8)}px;
 `;
 
-export const ButtonText = styled.Text`
-    font-size: 20px;
-    font-weight: 800;
+export const MaskWrapper = styled.View`
+  flex: 1;
+  background-color: rgba(0, 0, 0, 0.6);
+`;
+
+export const Mask = styled(MotiView)`
+  background-color: rgba(0, 0, 0, 1);
+  width: 100%;
+  height: 100%;
+  transform-origin: left;
 `;
