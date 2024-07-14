83d83302dc1dff397850d75ed9712c733d9d30aa
Whatshisname303 <120993368+Whatshisname303@users.noreply.github.com>
2024-07-14 04:11:25 -0700

    Allow unfocusing search bar text input

--- components/Searchbar/index.tsx
+++ components/Searchbar/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Pressable, Text, TextInput } from "react-native";
+import React, { useEffect } from "react";
+import { Keyboard, ScrollView, TextInput } from "react-native";
 import { Bar, Container, SearchInput, SearchResultContainer } from "./styles";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useTheme } from "styled-components/native";
@@ -13,28 +13,38 @@ export default function Searchbar(props: Props) {
     const [inputText, updateInputText] = React.useState("");
     const inputRef = React.useRef<TextInput>(null);
 
+    useEffect(() => {
+        const subscription = Keyboard.addListener("keyboardDidHide", () => {
+            Keyboard.dismiss();
+        });
+        return () => subscription.remove();
+    })
+
     const isFocused = inputRef.current?.isFocused();
 
     return (
         <Container>
-            <Bar
-                style={{
-                    borderColor: isFocused ? theme.colors.primaryMain : "#BC808C",
-                    borderBottomLeftRadius: isFocused ? 0 : 32,
-                    borderBottomRightRadius: isFocused ? 0 : 32,
-                    shadowColor: isFocused ? "#FFE9B9" : "rgba(0,0,0,0)",
-                }}
-            >
-                <SearchInput
-                    placeholder="Where to?"
-                    placeholderTextColor={theme.colors.primary4}
-                    value={inputText}
-                    onChangeText={updateInputText}
-                    ref={inputRef}
-                />
-            </Bar>
-            <SearchResultContainer style={{ display: isFocused ? "flex" : "none" }}>
-            </SearchResultContainer>
+            <ScrollView>
+                <Bar
+                    style={{
+                        borderColor: isFocused ? theme.colors.primaryMain : "#BC808C",
+                        borderBottomLeftRadius: isFocused ? 0 : 32,
+                        borderBottomRightRadius: isFocused ? 0 : 32,
+                        shadowColor: isFocused ? "#FFE9B9" : "rgba(0,0,0,0)",
+                    }}
+                >
+                    <SearchInput
+                        placeholder="Where to?"
+                        cursorColor={theme.colors.primary4}
+                        placeholderTextColor={theme.colors.primary4}
+                        value={inputText}
+                        onChangeText={updateInputText}
+                        ref={inputRef}
+                    />
+                </Bar>
+                <SearchResultContainer style={{ display: isFocused ? "flex" : "none" }}>
+                </SearchResultContainer>
+            </ScrollView>
         </Container>
     )
 }
