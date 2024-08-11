3f01c583c89728799526a00b1711ca72b377125b
Whatshisname303 <120993368+Whatshisname303@users.noreply.github.com>
2024-08-11 11:37:46 -0700

    Move search results to separate container

--- components/Searchbar/index.tsx
+++ components/Searchbar/index.tsx
@@ -1,11 +1,13 @@
-import React, { useEffect } from "react";
+import React, { ComponentProps, useEffect } from "react";
 import { Keyboard, ScrollView, TextInput } from "react-native";
-import { Bar, BuildingAddress, BuildingButton, BuildingIcon, BuildingName, BuildingTextContainer, Container, MagnifyingGlass, SearchInput, SearchResultContainer } from "./styles";
+import { Bar, Container, MagnifyingGlass, SearchInput, SearchResultContainer } from "./styles";
 import { useTheme } from "styled-components/native";
 import magnifyingGlass from "../../assets/magnifying-glass.png";
-import house from "../../assets/m-house.png";
+import SearchResult from "./SearchResult";
 
-const TEMP_BUILDINGS = [ // set to buildingReults
+type BuildingInfo = ComponentProps<typeof SearchResult>['building']
+
+const TEMP_BUILDINGS: BuildingInfo[] = [
     {name: "Ralph Rapson Hall", address: "89 Church St SE, Minneapolis, MN 55455"},
     {name: "Akerman Hall", address: "110 Union St SE, Minneapolis, MN 55455"},
     {name: "Coffman Memorial Union", address: "300 Washington Avenue SE, Minneapolis"},
@@ -18,7 +20,7 @@ interface Props {
 export default function Searchbar(props: Props) {
     const theme = useTheme();
     const [inputText, updateInputText] = React.useState("");
-    const [buildingResults, setBuildingResults] = React.useState(TEMP_BUILDINGS); // switch to temp buildings to test
+    const [buildingResults, setBuildingResults] = React.useState<BuildingInfo[]>([]); // switch to TEMP_BUILDINGS to test
     const inputRef = React.useRef<TextInput>(null);
 
     useEffect(() => {
@@ -28,41 +30,33 @@ export default function Searchbar(props: Props) {
         return () => subscription.remove();
     })
 
-    const isFocused = inputRef.current?.isFocused();
+    const showResults = inputRef.current?.isFocused() && buildingResults.length > 0;
 
     return (
         <Container>
-            <ScrollView>
-                <Bar
-                    style={{
-                        borderColor: isFocused ? theme.colors.primaryMain : theme.colors.primary3,
-                        borderBottomLeftRadius: isFocused ? 0 : 32,
-                        borderBottomRightRadius: isFocused ? 0 : 32,
-                        shadowColor: isFocused ? theme.colors.secondary3 : theme.colors.contrast,
-                    }}
-                >
-                    <MagnifyingGlass source={magnifyingGlass} />
-                    <SearchInput
-                        placeholder="Where to?"
-                        cursorColor={theme.colors.primary4}
-                        placeholderTextColor={theme.colors.primary4}
-                        value={inputText}
-                        onChangeText={updateInputText}
-                        ref={inputRef}
-                    />
-                </Bar>
-                <SearchResultContainer style={{ display: isFocused ? "flex" : "none" }}>
-                    {buildingResults.map((building) => (
-                        <BuildingButton key={building.name}>
-                            <BuildingIcon source={house} />
-                            <BuildingTextContainer>
-                                <BuildingName>{building.name}</BuildingName>
-                                <BuildingAddress>{building.address}</BuildingAddress>
-                            </BuildingTextContainer>
-                        </BuildingButton>
-                    ))}
-                </SearchResultContainer>
-            </ScrollView>
+            <Bar
+                style={{
+                    borderColor: showResults ? theme.colors.primaryMain : theme.colors.primary3,
+                    borderBottomLeftRadius: showResults ? 0 : 32,
+                    borderBottomRightRadius: showResults ? 0 : 32,
+                    shadowColor: showResults ? theme.colors.secondary3 : theme.colors.contrast,
+                }}
+            >
+                <MagnifyingGlass source={magnifyingGlass} />
+                <SearchInput
+                    placeholder="Where to?"
+                    cursorColor={theme.colors.primary4}
+                    placeholderTextColor={theme.colors.primary4}
+                    value={inputText}
+                    onChangeText={updateInputText}
+                    ref={inputRef}
+                />
+            </Bar>
+            <SearchResultContainer style={{ display: showResults ? "flex" : "none" }}>
+                <ScrollView>
+                    {buildingResults.map(building => <SearchResult key={building.address} building={building} />)}
+                </ScrollView>
+            </SearchResultContainer>
         </Container>
     )
 }
--- components/Searchbar/styles.ts
+++ components/Searchbar/styles.ts
@@ -14,9 +14,6 @@ export const Bar = styled.View`
     padding: 16px 24px;
     border: 1px solid;
     border-radius: 32px;
-    shadowOpacity: 0.2;
-    shadowRadius: 3px;
-    elevation: 20;
 `
 
 export const MagnifyingGlass = styled.Image`
@@ -38,37 +35,4 @@ export const SearchResultContainer = styled.View`
     border-top-width: 0;
     border-bottom-left-radius: 8px;
     border-bottom-right-radius: 8px;
-`
-
-export const BuildingButton = styled.View`
-    width: 100%;
-    height: 80px;
-    padding: 24px;
-    display: flex;
-    flex-direction: row;
-`
-
-export const BuildingIcon = styled.Image`
-    width: 16px;
-    height: 16px;
-    margin-right: 16px;
-`
-
-export const BuildingTextContainer = styled.View`
-    flex: 1;
-`
-
-export const BuildingName = styled.Text`
-    font-size: 16px;
-    font-weight: 600;
-    line-height: 20.16px;
-    color: ${({theme}) => theme.colors.primary5};
-    `
-
-export const BuildingAddress = styled.Text`
-    font-size: 16px;
-    font-weight: 500;
-    line-height: 20.16px;
-    height: 20.16px;
-    color: ${({theme}) => theme.colors.gray1};
 `
