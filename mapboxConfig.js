a05e1e2ed08128ea960e4cd02ea2714f710bacdf
Faizan Amin <123916246+FaizanAmin2266@users.noreply.github.com>
2025-02-25 18:13:10 -0600

    Fixed Mapbox, frontend uses .env file (#92)

--- mapboxConfig.js
+++ mapboxConfig.js
@@ -1,1 +1,1 @@
-export const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiZnV6enkyMjY2IiwiYSI6ImNtMDMyam5jbzA3eWoyam9obG56dDQ0eXgifQ.I28DwMp0Sh1te2N7ODmfgQ';
+export const MAPBOX_ACCESS_TOKEN = process.env.EXPO_PUBLIC_MAPBOX_TOK;
