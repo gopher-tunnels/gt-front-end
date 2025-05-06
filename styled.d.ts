030ca2f56208264b66bc76a8023d76df16034e2d
Victor Hofstetter <victorbarroshof@gmail.com>
2025-05-06 12:03:06 -0500

    fix styled-components theme type inference; (#104)
    rename extensions declaration file

---
+++ styled.d.ts
@@ -0,0 +1,52 @@
+import "styled-components/native";
+
+export interface CustomTheme {
+  name: string;
+  fontSizes: {
+    mini: number;
+    normal: number;
+    medium: number;
+  };
+  colors: {
+    primary1: string;
+    primary2: string;
+    primary3: string;
+    primary4: string;
+    primary5: string;
+    primaryMain: string;
+    primary7: string;
+    primary8: string;
+    primary9: string;
+    primaryDarkest: string;
+
+    secondary1: string;
+    secondary2: string;
+    secondary3: string;
+    secondary4: string;
+    secondary5: string;
+    secondaryMain: string;
+    secondary7: string;
+    secondary8: string;
+    secondary9: string;
+    secondaryDarkest: string;
+
+    contrast: string;
+
+    neutral: string;
+
+    tunnel1: string;
+    tunnel2: string;
+
+    skyway1: string;
+    skyway2: string;
+
+    sidewalk1: string;
+    sidewalk2: string;
+
+    gray1: string;
+  };
+}
+
+declare module "styled-components/native" {
+  export interface DefaultTheme extends CustomTheme {}
+}
